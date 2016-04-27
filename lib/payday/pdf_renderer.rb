4b05c8f21c198676734139629af42227d977869a
Blair Williams <blair@caseproof.com>
2016-04-26 19:34:09 -0600

    Now were doing this

--- lib/payday/pdf_renderer.rb
+++ lib/payday/pdf_renderer.rb
@@ -87,7 +87,7 @@ module Payday
       title = I18n.t "payday.invoice.title", default: "INVOICE"
       pdf.font("Helvetica-Bold") do
         pdf.fill_color "000000"
-        pdf.draw_text title, size: 35, at: [0,pdf.bounds.top]
+        pdf.draw_text title, size: 35, at: [0,pdf.bounds.top + 25]
       end
 
       pdf.move_cursor_to(pdf.bounds.top - 100)
